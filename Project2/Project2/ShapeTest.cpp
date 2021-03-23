032752055b100dc1173007fd0184957466876f91
Yali Wang <58898952+YaliWang2019@users.noreply.github.com>
2021-03-23 02:42:56 -0800

    Polygon, triangle, square, rectangle, spacer

--- Project2/Project2/ShapeTest.cpp
+++ Project2/Project2/ShapeTest.cpp
@@ -13,9 +13,9 @@ using std::vector;
 using std::make_unique;
 using std::move;
 
-TEST_CASE("Circle(double radius)") {
+TEST_CASE("Circle") {
 	Circle c1(0);
-	SECTION("Point")
+	SECTION("Empty")
 	{
 		REQUIRE(c1.getWidth() == 0);
 		REQUIRE(c1.getHeight() == 0);
@@ -24,7 +24,7 @@ TEST_CASE("Circle(double radius)") {
 	Circle c2(10);
 	Circle c3(3.14);
 	Circle c4(19971129);
-	SECTION("Single-Parameter Constructor")
+	SECTION("Circles with different radius")
 	{
 		REQUIRE(c2.getWidth() == 20);
 		REQUIRE(c2.getHeight() == 20);
@@ -47,19 +47,73 @@ TEST_CASE("Circle(double radius)") {
 
 }
 
-TEST_CASE("Rectangle(double height, double width)") {
-	Rectangle r0(3,4);
+TEST_CASE("Polygon") {
+	SECTION("Triangle")
+	{
+		Polygon p(3, 50);
+		REQUIRE(p.getWidth() == 50);
+	}
+	SECTION("Triangle Drawing")
+	{
+		Polygon p(3, 50);
+
+		REQUIRE(p.getPostScript() == "/length 50.000000 def\n" \
+			"/nSides 3 def\n" \
+			"/angle { 360 nSides div } def\n" \
+			"gsave\n" \
+			"25.000000 21.650634 translate\n"
+			"newpath\n" \
+			"0 0 moveto\n" \
+			"0 angle 360 {\n" \
+			"length 0 lineto\n" \
+			"length 0 translate\n" \
+			"angle rotate\n" \
+			"} for\n" \
+			"closepath\n" \
+			"stroke\n" \
+			"grestore\n");
+	}
+
+	SECTION("Square")
+	{
+		Polygon p(4, 50);
+		REQUIRE(p.getWidth() == 50);
+	}
+	SECTION("Square Drawing")
+	{
+		Polygon p(4, 50);
+
+		REQUIRE(p.getPostScript() == "/length 50.000000 def\n" \
+			"/nSides 4 def\n" \
+			"/angle { 360 nSides div } def\n" \
+			"gsave\n" \
+			"25.000000 25.000000 translate\n"
+			"newpath\n" \
+			"0 0 moveto\n" \
+			"0 angle 360 {\n" \
+			"length 0 lineto\n" \
+			"length 0 translate\n" \
+			"angle rotate\n" \
+			"} for\n" \
+			"closepath\n" \
+			"stroke\n" \
+			"grestore\n");
+	}
+}
+
+TEST_CASE("Rectangle") {
+	Rectangle r0(3, 4);
 	SECTION("Point")
 	{
 		REQUIRE(r0.getWidth() == 3);
 		REQUIRE(r0.getHeight() == 4);
-		
+
 	}
 
-	Rectangle r1(3,4);
-	Rectangle r2(5,20);
-	Rectangle r3(100,999);
-	SECTION("Single-Parameter Constructor")
+	Rectangle r1(3, 4);
+	Rectangle r2(5, 20);
+	Rectangle r3(100, 999);
+	SECTION("Rectangles with different width and height")
 	{
 		REQUIRE(r1.getWidth() == 3);
 		REQUIRE(r1.getHeight() == 4);
@@ -70,11 +124,11 @@ TEST_CASE("Rectangle(double height, double width)") {
 		REQUIRE(r3.getWidth() == 100);
 		REQUIRE(r3.getHeight() == 999);
 	}
-	SECTION("PostScript Drawing")
+	SECTION("Rectangle Drawing")
 	{
-		string testC1 = "newpath 0 0 moveto 0 4 lineto 3 0 lineto 0 -4 lineto closepath stroke\n";
-		string testC2 = "newpath 0 0 moveto 0 20 lineto 5 0 lineto 0 -20 lineto closepath stroke\n";
-		string testC3 = "newpath 0 0 moveto 0 999 lineto 100 0 lineto 0 999 lineto closepath stroke\n";
+		string testC1 = "newpath \n 0 0 moveto\n 0 4.000000 lineto\n 3.000000 0 lineto\n 0 -4.000000 lineto\n closepath\n stroke\n";
+		string testC2 = "newpath \n 0 0 moveto\n 0 20.000000 lineto\n 5.000000 0 lineto\n 0 -20.000000 lineto\n closepath\n stroke\n";
+		string testC3 = "newpath \n 0 0 moveto\n 0 999.000000 lineto\n 100.000000 0 lineto\n 0 -999.000000 lineto\n closepath\n stroke\n";
 		REQUIRE(r1.getPostScript() == testC1);
 		REQUIRE(r2.getPostScript() == testC2);
 		REQUIRE(r3.getPostScript() == testC3);
@@ -82,19 +136,19 @@ TEST_CASE("Rectangle(double height, double width)") {
 
 }
 
-TEST_CASE("Spacer(double height, double width)") {
-	Spacer r0(3,4);
-	SECTION("Point")
+TEST_CASE("Spacer") {
+	Spacer r0(3, 4);
+	SECTION("Simple Spacer")
 	{
 		REQUIRE(r0.getWidth() == 3);
 		REQUIRE(r0.getHeight() == 4);
-		
+
 	}
 
-	Spacer r1(3,4);
-	Spacer r2(5,20);
-	Spacer r3(100,999);
-	SECTION("Single-Parameter Constructor")
+	Spacer r1(3, 4);
+	Spacer r2(5, 20);
+	Spacer r3(100, 999);
+	SECTION("Spacers with different width and height")
 	{
 		REQUIRE(r1.getWidth() == 3);
 		REQUIRE(r1.getHeight() == 4);
@@ -105,15 +159,14 @@ TEST_CASE("Spacer(double height, double width)") {
 		REQUIRE(r3.getWidth() == 100);
 		REQUIRE(r3.getHeight() == 999);
 	}
-	SECTION("PostScript Drawing")
+	SECTION("Spacer Drawing")
 	{
-		string testC1 = "255 255 255 setrgbcolor newpath 0 0 moveto 0 4 lineto 3 0 lineto 0 -4 lineto closepath stroke\n";
-		string testC2 = "255 255 255 setrgbcolor newpath 0 0 moveto 0 20 lineto 5 0 lineto 0 -20 lineto closepath stroke\n";
-		string testC3 = "255 255 255 setrgbcolor newpath 0 0 moveto 0 999 lineto 100 0 lineto 0 999 lineto closepath stroke\n";
+		string testC1 = "255 255 255 setrgbcolor\n newpath\n 0 0 moveto\n 0 4.000000 lineto\n 3.000000 0 lineto\n 0 -4.000000 lineto\n closepath\n stroke\n";
+		string testC2 = "255 255 255 setrgbcolor\n newpath\n 0 0 moveto\n 0 20.000000 lineto\n 5.000000 0 lineto\n 0 -20.000000 lineto\n closepath\n stroke\n";
+		string testC3 = "255 255 255 setrgbcolor\n newpath\n 0 0 moveto\n 0 999.000000 lineto\n 100.000000 0 lineto\n 0 -999.000000 lineto\n closepath\n stroke\n";
 		REQUIRE(r1.getPostScript() == testC1);
 		REQUIRE(r2.getPostScript() == testC2);
 		REQUIRE(r3.getPostScript() == testC3);
 	}
 
 }
-
