427b4a2b393720deff0670f08d27fe149ade503d
Yali Wang <58898952+YaliWang2019@users.noreply.github.com>
2021-03-23 02:39:10 -0800

    Update Shape.hpp
    Passed tests for polygon, triangle, square, rectangle, spacer.

--- Project2/Project2/Shape.hpp
+++ Project2/Project2/Shape.hpp
@@ -10,6 +10,8 @@ using std::unique_ptr;
 #include <string>
 using std::string;
 
+const float pi = 3.14159265359;
+
 class Shape {
 public:
 	using Shape_ptr = std::unique_ptr<Shape>;
@@ -31,7 +33,36 @@ private:
 	double _radius;
 	double _height;
 	double _width;
-	// unique_ptr<DrawCircleStrategy> drawing; 
+
+};
+
+class Polygon : public Shape {
+public:
+	Polygon(int numSides, double sideLength);
+	double getWidth() const noexcept;
+	double getHeight() const noexcept;
+	std::string getPostScript() const override;
+
+private:
+	double _height;
+	double _width;
+	int _numSides;
+	double _sideLength;
+
+};
+
+class Triangle : public Polygon {
+public:
+public:
+	explicit Triangle(double sideLength) :Polygon(3, sideLength) {};
+	~Triangle() override = default;
+};
+
+class Square : public Polygon {
+public:
+public:
+	explicit Square(double sideLength) :Polygon(4, sideLength) {};
+	~Square() override = default;
 };
 
 class Rectangle : public Shape {
@@ -46,12 +77,13 @@ public:
 private:
 	double _height;
 	double _width;
+
 };
 
-class Spacer // : public Shape //A Shape?? 
-{ 
-	public:
-	Spacer( double _width, double _height);
+class Spacer : public Shape
+{
+public:
+	Spacer(double _width, double _height);
 
 	double getWidth() const noexcept;
 	double getHeight() const noexcept;
@@ -63,23 +95,6 @@ private:
 	double _width;
 };
 
-class Square : public Shape {
-	Square(double sideLength);
-	double getLength() const noexcept;
-	string getPostScript() const override;
-
-private:
-	double _length;
-};
-
-class Triangle : public Shape {
-	Triangle(double sideLength);
-	double getLength() const noexcept;
-	string getPostScript() const override;
-
-private:
-	double _length;
-};
 
 /*class Rotated : public Shape {
 	Rotated(Shape_ptr shape, RotationAngle angle);
@@ -99,19 +114,19 @@ private:
 	Shape_ptr _shape;
 };
 
-class Layered : public Shape {
-	Layered(Shape_ptr shape, Shape shape2 /*, shape3, ...*/);
-	
-	string getPostScript() const override;
-};
+//class Layered : public Shape {
+//	Layered(Shape_ptr shape, Shape shape2 /*, shape3, ...*/);
+//
+//	string getPostScript() const override;
+//};
 
-class Vertical : public Shape {
-	Vertical(Shape_ptr shape, Shape shape2 /*, shape3, ...*/);
-	string getPostScript() const override;
-};
+//class Vertical : public Shape {
+//	Vertical(Shape_ptr shape, Shape shape2 /*, shape3, ...*/);
+//	string getPostScript() const override;
+//};
 
-class Horizontal : public Shape {
-		Horizontal(Shape_ptr shape, Shape shape2 /*, shape3, ...*/);
-	string getPostScript() const override;
-};
+//class Horizontal : public Shape {
+//	Horizontal(Shape_ptr shape, Shape shape2 /*, shape3, ...*/);
+//	string getPostScript() const override;
+//};
 #endif //!SHAPE_HPP
