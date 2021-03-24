abbfa3b08cb2b176869a7392e0b6bc5b694a3f30
Rose Peters <rmpeters3@alaska.edu>
2021-03-24 06:24:39 -0800

    Drawing shape draw.cpp

--- Project2/Project2/draw.cpp
+++ Project2/Project2/draw.cpp
@@ -1,8 +1,23 @@
 // draw.cpp
 // Hunter Barndt, Yali Wang, Rose Peters
 
+
 #include "Shape.hpp"
 #include "CompoundShape.h"
+
+#include <iostream>
+using std::endl;
+#include <fstream>
+
 int main() {
-	return 0;
+	Circle c1(50);
+	Spacer s1(50,50);
+	Triangle t1(50);
+	Rectangle r1(50, 50);
+	std::ofstream psfile;
+	psfile.open("shape.ps");
+	psfile << c1.getPostScript() << endl << s1.getPostScript();
+	psfile << r1.getPostScript();
+	psfile << std::endl << "showpage";
+
 }
