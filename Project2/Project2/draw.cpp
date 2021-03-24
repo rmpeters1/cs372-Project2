98328203752028eafb6c68c0b81e174e04247a02
Rose Peters <55459783+rmpeters1@users.noreply.github.com>
2021-03-24 15:28:56 -0800

    draw.cpp

--- Project2/Project2/draw.cpp
+++ Project2/Project2/draw.cpp
@@ -7,18 +7,34 @@
 #include <iostream>
 
 int main() {
-	Circle c1(50);
-	Spacer s1(50, 50);
-	Rectangle r1(50, 50);
-	Polygon p1(5, 50);
-	//Translate t1(300, 300);
+    Circle c1(50);
+    Spacer s1(50, 50);
+    Spacer s2(0, 50);
+    Spacer s3(50, 0);
+    Rectangle r1(20, 100);
+    Rectangle r2(100, 50);
+    Polygon p1(5, 50);
+    Polygon p2(7, 50);
+    Polygon pT(3, 50);
+    Polygon pS(4, 50)
+        ;    Triangle t1(3);
+    //Translate t1(300, 300);
 
-	std::ofstream psfile;
-	psfile.open("shapes.ps");
+    std::ofstream psfile;
+    psfile.open("shapes.ps");
 
-	//psfile << t1.getPostScript();
-	psfile << c1.getPostScript();
-	psfile << s1.getPostScript();
-	psfile << r1.getPostScript();
-	psfile << p1.getPostScript() +"showpage";
+    //psfile << t1.getPostScript();
+    psfile << s1.getPostScript();
+    psfile << c1.getPostScript();
+    psfile << s2.getPostScript();
+    psfile << r1.getPostScript();
+    psfile << s3.getPostScript();
+    psfile << p1.getPostScript();
+    psfile << s3.getPostScript();
+    psfile << s1.getPostScript();
+    psfile << pT.getPostScript();
+    psfile << s2.getPostScript();
+    psfile << pS.getPostScript();
+    psfile << s1.getPostScript();
+    psfile << p2.getPostScript() << "showpage";
 }
