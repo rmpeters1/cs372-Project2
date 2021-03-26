d79422b58f5c3666f6a28b5d760e816a498c2464
Rose Peters <55459783+rmpeters1@users.noreply.github.com>
2021-03-25 23:13:40 -0800

    vector of unique_ptr to draw

--- Project2/Project2/draw.cpp
+++ Project2/Project2/draw.cpp
@@ -7,35 +7,19 @@
 #include <iostream>
 
 int main() {
-    Circle c1(50);
-    Spacer s1(50, 50);
-    Spacer s2(0, 50);
-    Spacer s3(50, 0);
-    Rectangle r1(20, 100);
-    Rectangle r2(100, 50);
-    Polygon p1(5, 50);
-    Polygon p2(7, 50);
-    Polygon pT(3, 50);
-    Polygon pS(4, 50);
-    Triangle t1(3);
-    //TranslatedSpace t1(300, 300);
-    //StackedShape ss1(c1)
+	std::ofstream psfile;
+	psfile.open("shapes.ps");
+	using Shapes = std::vector<std::unique_ptr<Shape>>;
+	Shapes shapes;
+	shapes.push_back(std::make_unique<Circle>(2));
+	shapes.push_back(std::make_unique<Square>(5));
+	
 
-    std::ofstream psfile;
-    psfile.open("shapes.ps");
+	//Drawing all shapes
+	psfile << getPostScript(shapes);
 
-    //psfile << t1.getPostScript();
-    psfile << s1.getPostScript();
-    psfile << c1.getPostScript();
-    psfile << s2.getPostScript();
-    psfile << r1.getPostScript();
-    psfile << s3.getPostScript();
-    psfile << p1.getPostScript();
-    psfile << s3.getPostScript();
-    psfile << s1.getPostScript();
-    psfile << pT.getPostScript();
-    psfile << s2.getPostScript();
-    psfile << pS.getPostScript();
-    psfile << s1.getPostScript();
-    psfile << p2.getPostScript() << "showpage";
+
+
+
+	
 }
