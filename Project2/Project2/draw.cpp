74f1ba51959ada0e749c0325c48cace8771394a4
Rose Peters <55459783+rmpeters1@users.noreply.github.com>
2021-04-29 10:41:47 -0800

    Compiles but postscript is missing something

--- Project2/Project2/draw.cpp
+++ Project2/Project2/draw.cpp
@@ -9,32 +9,38 @@
 int main() {
 	std::ofstream psfile;
 	psfile.open("shapes.ps");
-	
+
 	// Container for all shapes that we will draw;
+	using Shapes = std::vector<std::unique_ptr<Shape>>;
 	Shapes shapes;
 
-	// Shapes Created
-	Shape_ptr c1 = std::make_unique<Circle>(2);						// Circle
-	Shape_ptr s1 = std::make_unique<Square>(5);						// Square
-	Shape_ptr t1 = std::make_unique<Polygon>(3);					// Triangle
-	Shape_ptr ts1 = std::make_unique<TranslatedShape>(200, 200);
-	Shape_ptr ts2 = std::make_unique<TranslatedShape>(500, 500);
 
-	std::vector<Shape_ptr> layeredshapes;
+	// Shapes Created
+	Shape::Shape_ptr c1 = std::make_unique<Circle>(2);						// Circle
+	Shape::Shape_ptr s1 = std::make_unique<Square>(5);						// Square
+	//Shape::Shape_ptr t1 = std::make_unique<Polygon>(3);					// Triangle
+	Shape::Shape_ptr ts1 = std::make_unique<TranslatedShape>(200, 200);
+	Shape::Shape_ptr ts2 = std::make_unique<TranslatedShape>(500, 500);
+
+	std::vector<Shape::Shape_ptr> layeredshapes;
 	layeredshapes.push_back(c1);
-	layeredshapes.push_back(t1);
-	Shape_ptr ls1 = std::make_unique<LayeredShape>(layeredshapes);	// Circle on Triangle
+	//layeredshapes.push_back(t1);
+	Shape::Shape_ptr ls1 = std::make_unique<LayeredShape>(layeredshapes);	// Circle on Triangle
 
 	// Pushing back all shapes that we want drawn
-	shapes.push_back(ts1);
-	shapes.push_back(c1);
-	shapes.push_back(s1);
-	shapes.push_back(ts2);
-	shapes.push_back(ls1);
+	shapes.push_back(std::make_unique<TranslatedShape>(200, 200));
+	shapes.push_back(std::make_unique<Circle>(2));
+	shapes.push_back(std::make_unique<Square>(5));
+	shapes.push_back(std::make_unique<TranslatedShape>(500, 500));
+	shapes.push_back(std::make_unique<LayeredShape>(layeredshapes));
+
 
 	//Drawing all shapes
 	for (auto const& s : shapes) {
-		psfile << s.get().getPostScript(shapes);
+		psfile << s->getPostScript();
 	}
+
+	psfile << std::endl << "showpage";
+
 
 }
